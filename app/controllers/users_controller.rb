74b4af2a1b4038a580de5c34f2c7ba86eebcd89f
Matthew Alon <alonmatthew94@gmail.com>
2016-12-21 10:45:25 -0800

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update_attributes(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    current_user.destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+private
+  def user_params
+  return params.require(:user).permit(:username, :email, :password, :bio, :password_confirmation, :profile_img)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'users#index'
+  resources :users
+
+  delete '/logout' => 'sessions#destroy', as: :logout
+  resources :sessions, only: [:new, :create]
+end
